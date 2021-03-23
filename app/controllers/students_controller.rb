8f04f7d709127f704267cf0405f4c38700ff4f4b
danainjax <danainjax@gmail.com>
2021-03-23 10:37:25 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,11 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def create
+    #create a new student, save the student info in the params with keys/values submitted in form
+    #redirect somewhere
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
+  end
+
 end
